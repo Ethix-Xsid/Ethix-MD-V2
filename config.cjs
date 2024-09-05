cd423e7dbe11c963e17f9eef1d5dd1b404660f70
Ethix-Xsid <145905667+Ethix-Xsid@users.noreply.github.com>
2024-09-05 07:46:05 +0530

    ETHIX-MD-V2

--- config.cjs
+++ config.cjs
@@ -4,6 +4,7 @@ require("dotenv").config();
 
 const config = {
   SESSION_ID: process.env.SESSION_ID || "Your Session Id",
+  PREFIX: process.env.PREFIX || '.',
   AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true, 
   AUTO_DL: process.env.AUTO_DL !== undefined ? process.env.AUTO_DL === 'true' : false,
   AUTO_READ: process.env.AUTO_READ !== undefined ? process.env.AUTO_READ === 'true' : false,
