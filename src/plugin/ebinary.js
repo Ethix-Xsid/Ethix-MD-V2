c159209a6f506db79b8418b21e356ac464f3de6a
Ethix-Xsid <145905667+Ethix-Xsid@users.noreply.github.com>
2024-09-05 08:18:54 +0530

    update src/plugin/ebinary.js

--- src/plugin/ebinary.js
+++ src/plugin/ebinary.js
@@ -1,10 +1,10 @@
 import { eBinary } from '../../lib/binary.cjs';
+import config from '../../config.cjs';
 
 const ebinary = async (m, gss) => {
-const prefixMatch = m.body.match(/^[\\/!#.]/);
-  const prefix = prefixMatch ? prefixMatch[0] : '/';
-  const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
-  const text = m.body.slice(prefix.length + cmd.length).trim();
+const prefix = config.PREFIX;
+const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
+const text = m.body.slice(prefix.length + cmd.length).trim();
 
   const validCommands = ['ebinary'];
 
