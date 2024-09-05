797e05772338569264ff4b5f8f8023e71ab53e88
Ethix-Xsid <145905667+Ethix-Xsid@users.noreply.github.com>
2024-09-05 08:18:06 +0530

    update src/plugin/dbinary.js

--- src/plugin/dbinary.js
+++ src/plugin/dbinary.js
@@ -1,10 +1,10 @@
 import { dBinary } from '../../lib/binary.cjs';
+import config from '../../config.cjs';
 
 const dbinary = async (m, gss) => {
-const prefixMatch = m.body.match(/^[\\/!#.]/);
-  const prefix = prefixMatch ? prefixMatch[0] : '/';
-  const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
-  const text = m.body.slice(prefix.length + cmd.length).trim();
+const prefix = config.PREFIX;
+const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
+const text = m.body.slice(prefix.length + cmd.length).trim();
 
   const validCommands = ['dbinary'];
 
