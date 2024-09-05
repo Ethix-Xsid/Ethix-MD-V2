3d695296a4ab4ec827fe60315989dcbd0e0bb9ff
Ethix-Xsid <145905667+Ethix-Xsid@users.noreply.github.com>
2024-09-05 08:20:42 +0530

    update src/plugin/fetch.js

--- src/plugin/fetch.js
+++ src/plugin/fetch.js
@@ -1,10 +1,10 @@
 import fetch from 'node-fetch';
+import config from '../../config.cjs';
 
 const fetchData = async (m, Matrix) => {
-  const prefixMatch = m.body.match(/^[\\/!#.]/);
-  const prefix = prefixMatch ? prefixMatch[0] : '/';
-  const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
-  const text = m.body.slice(prefix.length + cmd.length).trim();
+  const prefix = config.PREFIX;
+const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
+const text = m.body.slice(prefix.length + cmd.length).trim();
 
   const validCommands = ['fetch', 'get', 'api'];
 
