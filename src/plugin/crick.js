b3379049e8d318afb566cc0b077bcd04e80cf9d2
Ethix-Xsid <145905667+Ethix-Xsid@users.noreply.github.com>
2024-09-05 08:17:38 +0530

    update src/plugin/crick.js

--- src/plugin/crick.js
+++ src/plugin/crick.js
@@ -1,10 +1,10 @@
+import config from '../../config.cjs';
 import axios from 'axios';
 
 const cricketScore = async (m, Matrix) => {
-  const prefixMatch = m.body.match(/^[\\/!#.]/);
-  const prefix = prefixMatch ? prefixMatch[0] : '/';
-  const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
-  const text = m.body.slice(prefix.length + cmd.length).trim();
+  const prefix = config.PREFIX;
+const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
+const text = m.body.slice(prefix.length + cmd.length).trim();
 
   const validCommands = ['score', 'crick', 'crickterscore', 'cricket'];
 
