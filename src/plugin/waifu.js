3005dfd1c00acb17adc7303f9ceb35fe29422438
Ethix-Xsid <145905667+Ethix-Xsid@users.noreply.github.com>
2024-09-05 08:44:59 +0530

    update src/plugin/waifu.js

--- src/plugin/waifu.js
+++ src/plugin/waifu.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
-
+import config from '../../config.cjs';
 
 const stickerCommandHandler = async (m, gss) => {
-  const prefixMatch = m.body.match(/^[\\/!#.]/);
-  const prefix = prefixMatch ? prefixMatch[0] : '/';
-  const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
+  const prefix = config.PREFIX;
+const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
+const text = m.body.slice(prefix.length + cmd.length).trim();
   
   const stickerCommands = ['cry', 'kiss', 'kill', 'kick', 'hug', 'pat', 'lick', 'bite', 'yeet', 'bully', 'bonk', 'wink', 'poke', 'nom', 'slap', 'smile', 'wave', 'awoo', 'blush', 'smug', 'dance', 'happy', 'sad', 'cringe', 'cuddle', 'shinobu', 'handhold', 'glomp', 'highfive'];
 
