3d34e597e0598f6fa2a06e3f0c2564e25d7c145e
Ethix-Xsid <145905667+Ethix-Xsid@users.noreply.github.com>
2024-10-27 16:46:06 +0530

    update src/event/handler.js

--- src/event/handler.js
+++ src/event/handler.js
@@ -39,19 +39,6 @@ const Handler = async (chatUpdate, sock, logger) => {
         const prefix = prefixMatch ? prefixMatch[0] : '/';
         const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
         const text = m.body.slice(prefix.length + cmd.length).trim();
-
-        if (m.key && m.key.remoteJid === 'status@broadcast' && config.AUTO_STATUS_SEEN) {
-    try {
-        await sock.readMessages([m.key]);
-        if (config.AUTO_STATUS_REPLY) {
-            const customMessage = config.STATUS_READ_MSG;
-            await sock.sendMessage(m.from, { text: customMessage }, { quoted: m });
-        }
-    } catch (error) {
-        console.error('Error reading status or sending message:', error);
-    }
-}
-
         const botNumber = await sock.decodeJid(sock.user.id);
         const ownerNumber = config.OWNER_NUMBER + '@s.whatsapp.net';
         let isCreator = false;
