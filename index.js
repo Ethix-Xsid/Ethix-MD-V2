08f35dd2d0c0cb2103f7a541c1d15267e04047bc
Ethix-Xsid <145905667+Ethix-Xsid@users.noreply.github.com>
2024-10-27 16:50:25 +0530

    update index.js

--- index.js
+++ index.js
@@ -131,6 +131,26 @@ async function start() {
                 console.error('Error during auto reaction:', err);
             }
         });
+        
+        Matrix.ev.on('messages.upsert', async (chatUpdate) => {
+    try {
+        const mek = chatUpdate.messages[0];
+        const fromJid = mek.key.participant || mek.key.remoteJid;
+        if (!mek || !mek.message) return;
+        if (mek.key.fromMe) return;
+        if (mek.message?.protocolMessage || mek.message?.ephemeralMessage) return;
+        if (mek.key && mek.key.remoteJid === 'status@broadcast' && config.AUTO_STATUS_SEEN) {
+        await Matrix.readMessages([mek.key]);   
+            if (config.AUTO_STATUS_REPLY) {
+                const customMessage = config.READ_MSG || '✅ Auto Status Seen Bot By Ethix-MD-V2';
+                await Matrix.sendMessage(fromJid, { text: customMessage }, { quoted: mek });
+            }
+        }
+    } catch (err) {
+        console.error('Error handling messages.upsert event:', err);
+    }
+});
+
     } catch (error) {
         console.error('Critical Error:', error);
         process.exit(1);
